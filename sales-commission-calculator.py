1b2ce999f7a993eded0be353cea633b6f45786d3
SayedOmarHashimi <omarhashimi12@gmail.com>
2025-04-02 17:24:55 -0700

    sales-commission-calculator.py
    A tool that calculates sales commissions and salary adjustments based on performance.

--- sales-commission-calculator.py
+++ sales-commission-calculator.py
@@ -1,35 +1,36 @@
 keep_going = 'y'
 
 while keep_going == 'y':
+  name = input('Enter Name: ')
   sales = float(input('Enter Sales: '))
-  comm_rate = float(input('Enter Commission Rate: '))
-  commission = sales * comm_rate
-  print('Commission Rate: $',round(commission,2))
-  keep_going = input('Enter (y) to keep going, otherwise enter (n): ')
-print('Thank you for using our service!')
-
-name = input('Enter the name of the employee ')
-sales = float(input('Enter your sales amount '))
-
-if sales >= 1 and sales <= 100:
-  print('There is no commission')
-elif sales >= 101 and sales <= 250:
-  salary = round(sales + (sales * 0.10),2)
-  print('Name            Salary')
-  print('----------------------')
-  print(name,'          ',salary)
-elif sales >= 251 and sales <= 500:
-  salary = round(sales + (sales * 0.15),2)
-  print('Name            Salary')
-  print('----------------------')
-  print(name,'          ',salary)
-elif sales >= 501 and sales <= 1000:
-  salary = round(sales + (sales * 0.20),2)
-  print('Name            Salary')
-  print('----------------------')
-  print(name,'          ',salary)
-elif sales > 1000:
-  print(name,'Your surprise is at the management office; Go meet your manager')
-else:
-  print('Enter a valid number.')
+  if sales >= 1 and sales <= 10000:
+    print('No Commission')
+  elif sales >= 10001 and sales <= 50000:
+    salary = round(sales + (sales * 0.10),2)
+    print()
+    print('NAME                    SALARY')
+    print('------------------------------')
+    print(name,'                  ',salary)
+    print()
+  elif sales >= 50001 and sales <= 100000:
+    print()
+    salary = round(sales + (sales * 0.15),2)
+    print('NAME                    SALARY')
+    print('------------------------------')
+    print(name,'                  ',salary)
+    print()
+  elif sales >= 100001 and sales <= 250000:
+    print()
+    salary = round(sales + (sales * 0.20),2)
+    print('NAME                    SALARY')
+    print('------------------------------')
+    print(name,'                  ',salary)
+    print()
+  else:
+    print()
+    print('Go see your manager!')
+    print()
+  keep_going = input('To continue press (y), otherwise please press (n): ')
+print()
+print('Thank you for using our service, have a good day!')
 
